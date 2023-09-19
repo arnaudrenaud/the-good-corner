6a9aaba020c8f60e098ae4f63bde827f8bb1f66a
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-09-19 16:52:25 +0200

    Add POST /ads to create new ad
    Ad will not be persisted beyond server restart.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { ads } from "./ads";
 
 const server = express();
+server.use(express.json());
 
 // Hello world
 server.get("/", (request, response) => {
@@ -11,6 +12,14 @@ server.get("/", (request, response) => {
 // GET /ads
 server.get("/ads", (request, response) => {
   response.json({ ads });
+});
+
+// POST /ads
+server.post("/ads", (request, response) => {
+  const ad = request.body;
+
+  ads.push(ad);
+  response.json({ ad });
 });
 
 const PORT = 4000;
