2d2404d0bee03e3651a0ec85b4da0c5ab70fa1b2
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-09-26 10:56:34 +0200

    Rename class names starting with `ad` to `article`

--- web-app/src/pages/index.tsx
+++ web-app/src/pages/index.tsx
@@ -73,60 +73,69 @@ export default function Home() {
       <main className="main-content">
         <h2>Annonces récentes</h2>
         <section className="recent-ads">
-          <div className="ad-card-container">
-            <a className="ad-card-link" href="/ads/table">
-              <img className="ad-card-image" src="/images/table.webp" />
-              <div className="ad-card-text">
-                <div className="ad-card-title">Table</div>
-                <div className="ad-card-price">120 €</div>
+          <div className="article-card-container">
+            <a className="article-card-link" href="/ads/table">
+              <img className="article-card-image" src="/images/table.webp" />
+              <div className="article-card-text">
+                <div className="article-card-title">Table</div>
+                <div className="article-card-price">120 €</div>
               </div>
             </a>
           </div>
-          <div className="ad-card-container">
-            <a className="ad-card-link" href="/ads/dame-jeanne">
-              <img className="ad-card-image" src="/images/dame-jeanne.webp" />
-              <div className="ad-card-text">
-                <div className="ad-card-title">Dame-jeanne</div>
-                <div className="ad-card-price">75 €</div>
-              </div>
-            </a>
-          </div>
-          <div className="ad-card-container">
-            <a className="ad-card-link" href="/ads/vide-poche">
-              <img className="ad-card-image" src="/images/vide-poche.webp" />
-              <div className="ad-card-text">
-                <div className="ad-card-title">Vide-poche</div>
-                <div className="ad-card-price">4 €</div>
-              </div>
-            </a>
-          </div>
-          <div className="ad-card-container">
-            <a className="ad-card-link" href="/ads/vaisselier">
-              <img className="ad-card-image" src="/images/vaisselier.webp" />
-              <div className="ad-card-text">
-                <div className="ad-card-title">Vaisselier</div>
-                <div className="ad-card-price">900 €</div>
-              </div>
-            </a>
-          </div>
-          <div className="ad-card-container">
-            <a className="ad-card-link" href="/ads/bougie">
-              <img className="ad-card-image" src="/images/bougie.webp" />
-              <div className="ad-card-text">
-                <div className="ad-card-title">Bougie</div>
-                <div className="ad-card-price">8 €</div>
-              </div>
-            </a>
-          </div>
-          <div className="ad-card-container">
-            <a className="ad-card-link" href="/ads/porte-magazine">
+          <div className="article-card-container">
+            <a className="article-card-link" href="/ads/dame-jeanne">
               <img
-                className="ad-card-image"
+                className="article-card-image"
+                src="/images/dame-jeanne.webp"
+              />
+              <div className="article-card-text">
+                <div className="article-card-title">Dame-jeanne</div>
+                <div className="article-card-price">75 €</div>
+              </div>
+            </a>
+          </div>
+          <div className="article-card-container">
+            <a className="article-card-link" href="/ads/vide-poche">
+              <img
+                className="article-card-image"
+                src="/images/vide-poche.webp"
+              />
+              <div className="article-card-text">
+                <div className="article-card-title">Vide-poche</div>
+                <div className="article-card-price">4 €</div>
+              </div>
+            </a>
+          </div>
+          <div className="article-card-container">
+            <a className="article-card-link" href="/ads/vaisselier">
+              <img
+                className="article-card-image"
+                src="/images/vaisselier.webp"
+              />
+              <div className="article-card-text">
+                <div className="article-card-title">Vaisselier</div>
+                <div className="article-card-price">900 €</div>
+              </div>
+            </a>
+          </div>
+          <div className="article-card-container">
+            <a className="article-card-link" href="/ads/bougie">
+              <img className="article-card-image" src="/images/bougie.webp" />
+              <div className="article-card-text">
+                <div className="article-card-title">Bougie</div>
+                <div className="article-card-price">8 €</div>
+              </div>
+            </a>
+          </div>
+          <div className="article-card-container">
+            <a className="article-card-link" href="/ads/porte-magazine">
+              <img
+                className="article-card-image"
                 src="/images/porte-magazine.webp"
               />
-              <div className="ad-card-text">
-                <div className="ad-card-title">Porte-magazine</div>
-                <div className="ad-card-price">45 €</div>
+              <div className="article-card-text">
+                <div className="article-card-title">Porte-magazine</div>
+                <div className="article-card-price">45 €</div>
               </div>
             </a>
           </div>
