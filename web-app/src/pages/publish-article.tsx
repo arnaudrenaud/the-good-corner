584c066ed643cdc876d2fee931551ad93c1ce6c2
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-09-28 12:02:32 +0200

    Create article on server when submitting form to publish article

--- web-app/src/pages/publish-article.tsx
+++ web-app/src/pages/publish-article.tsx
@@ -5,11 +5,33 @@ import {
   TextArea,
   TextField,
 } from "@/components/FormElements/Input/Input";
-import React from "react";
+import React, { useState } from "react";
+
+type PublishArticleFormData = {
+  title: string;
+  price: number | null;
+  description: string;
+  owner: string;
+};
 
 export default function PublishArticlePage() {
-  const createArticle = () => {
-    // call POST /articles on API
+  const [formData, setFormData] = useState<PublishArticleFormData>({
+    title: "",
+    price: null,
+    description: "",
+    owner: "",
+  });
+
+  const updateFormData = (partialFormData: Partial<PublishArticleFormData>) => {
+    setFormData({ ...formData, ...partialFormData });
+  };
+
+  const createArticle = async () => {
+    await fetch("/api/ads", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
   };
 
   return (
@@ -26,19 +48,43 @@ export default function PublishArticlePage() {
         </FormLabelWithField>
         <FormLabelWithField>
           Titre
-          <TextField type="text" required minLength={4} />
+          <TextField
+            type="text"
+            required
+            minLength={4}
+            onChange={(event) => {
+              updateFormData({ title: event.target.value });
+            }}
+          />
         </FormLabelWithField>
         <FormLabelWithField>
           Prix
-          <TextField type="number" required min={0} />
+          <TextField
+            type="number"
+            required
+            min={0}
+            onChange={(event) => {
+              updateFormData({ price: parseInt(event.target.value) });
+            }}
+          />
         </FormLabelWithField>
         <FormLabelWithField>
           Description
-          <TextArea />
+          <TextArea
+            onChange={(event) => {
+              updateFormData({ description: event.target.value });
+            }}
+          />
         </FormLabelWithField>
         <FormLabelWithField>
           Propriétaire
-          <TextField type="email" required />
+          <TextField
+            type="email"
+            required
+            onChange={(event) => {
+              updateFormData({ owner: event.target.value });
+            }}
+          />
         </FormLabelWithField>
         <PrimaryButton>Publier l'annonce</PrimaryButton>
       </Form>
