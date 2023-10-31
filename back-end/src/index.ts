587e900d09400f8a50ea1c6971fcc9a24ac3285f
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-10-31 14:52:51 +0100

    Set up GraphQL Apollo server with schema manually specified

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -6,6 +6,75 @@ import { isError } from "./utils";
 import Category from "./entities/category";
 import Tag from "./entities/tag";
 
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  type Ad {
+    id: ID
+    title: String
+    description: String
+    owner: String
+    price: Float
+    picture: String
+    location: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    ads(category: Int): [Ad]
+    
+  }
+`;
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    ads: (_: unknown, { category }: { category: number }) => {
+      return Ad.getAds(category ?? undefined);
+    },
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+
+const startApolloServer = async () => {
+  const { url } = await startStandaloneServer(apolloServer, {
+    listen: { port: 4001 },
+  });
+
+  console.log(`🚀  Server ready at: ${url}`);
+};
+
+startApolloServer();
+
 const dataSource = new DataSource({
   type: "sqlite",
   database: "db.sqlite",
