72a94e28d950b5ede7c1896b5efa1b6308f24433
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-11-29 09:46:12 +0100

    Set up Docker Compose with back-end and web-app

--- back-end/db-queries/insert-ads.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO
-  Ad (title, description, owner, price, categoryId)
-VALUES
-  ('Voiture', NULL, 'Laure', 3000, 2),
-  ('Jean', 'Comme neuf', 'Jean', 20, 1),
-  ('Chaise', '', 'Pierre', 100, 3),
-  ('Tapis', '', 'Arnaud', 150, 3);
