6ca34dae3d7d95ccf71bb7697de74a245b06f5a7
Arnaud Renaud <arnaud.renaud@gmail.com>
2024-02-06 17:23:47 +0100

    Write tests for function `getUserSessionIdFromCookie`

--- back-end/src/utils/cookie.ts
+++ back-end/src/utils/cookie.ts
@@ -15,5 +15,6 @@ export function setUserSessionIdInCookie(
 }
 
 export function getUserSessionIdFromCookie(req: IncomingMessage) {
-  return parse(req.headers.cookie || "").userSessionId as string | undefined;
+  const userSessionId = parse(req.headers.cookie || "").userSessionId;
+  return userSessionId || undefined;
 }
